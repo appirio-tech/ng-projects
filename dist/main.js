66ee01fa586b2a987da8d61cd752db98dc72960c
Alex Tran <vietnogi@gmail.com>
2015-10-16 13:00:52 -0700

    loader - build

--- dist/main.js
+++ dist/main.js
@@ -8,7 +8,7 @@
 
 }).call(this);
 
-angular.module("appirio-tech-ng-projects").run(["$templateCache", function($templateCache) {$templateCache.put("views/ng-projects.directive.html","<ul class=\"flex center wrap\"><li ng-repeat=\"project in vm.projects track by $index\"><header><a ui-sref=\"timeline({ workId: project.id })\"><h5>{{ project.name || \'Unnamed Project\' }}</h5></a><p class=\"type\">{{ vm.typeMap[project.projectType] }} - {{ project.status }}</p></header><main ng-if=\"project.status == \'Incomplete\'\" class=\"incomplete flex column middle center\"><div class=\"icon big warning\"></div><h6>Project submission incomplete</h6><button>continue setup</button></main><main ng-if=\"project.status == \'Submitted\'\" class=\"submitted flex column\"><div class=\"flex column middle center flex-grow\"><div class=\"icon big warning\"></div><h6>Project submitted</h6></div><footer><p>You are waiting for project approval</p><p>This should take about 1 day</p></footer></main><main ng-if=\"project.status == \'Estimate\'\" class=\"approved\"><hr/><h6>Project approved</h6><avatar class=\"icon big\"></avatar><p class=\"name\">Copilot name missing from API blah blah blah</p><p class=\"secondary\">Co-Pilot</p><a ui-sref=\"timeline({ workId: project.id })\" class=\"button action\">view quote</a></main><main ng-if=\"project.status == \'Launched\'\" class=\"launched flex column\"><div class=\"flex column middle center flex-grow\"><div class=\"icon big warning\"></div><h6>Project launched</h6></div><footer><p>Design work is in progress</p><p>Get design submissions in several days.</p></footer></main><main ng-if=\"project.status == \'Complete\'\" class=\"complete flex column middle center\"><div class=\"icon big warning\"></div><h6>Project complete!</h6><button>deploy my app</button></main></li></ul><div ng-if=\"vm.projects.length == 0\" class=\"no-projects\"><p>You have not started any projects.</p></div>");}]);
+angular.module("appirio-tech-ng-projects").run(["$templateCache", function($templateCache) {$templateCache.put("views/ng-projects.directive.html","<loader ng-show=\"vm.loading\"></loader><ul class=\"flex center wrap\"><li ng-repeat=\"project in vm.projects track by $index\"><header><a ui-sref=\"timeline({ workId: project.id })\"><h5>{{ project.name || \'Unnamed Project\' }}</h5></a><p class=\"type\">{{ vm.typeMap[project.projectType] }} - {{ project.status }}</p></header><main ng-if=\"project.status == \'Incomplete\'\" class=\"incomplete flex column middle center\"><div class=\"icon big warning\"></div><h6>Project submission incomplete</h6><button>continue setup</button></main><main ng-if=\"project.status == \'Submitted\'\" class=\"submitted flex column\"><div class=\"flex column middle center flex-grow\"><div class=\"icon big warning\"></div><h6>Project submitted</h6></div><footer><p>You are waiting for project approval</p><p>This should take about 1 day</p></footer></main><main ng-if=\"project.status == \'Estimate\'\" class=\"approved\"><hr/><h6>Project approved</h6><avatar class=\"icon big\"></avatar><p class=\"name\">Copilot name missing from API blah blah blah</p><p class=\"secondary\">Co-Pilot</p><a ui-sref=\"timeline({ workId: project.id })\" class=\"button action\">view quote</a></main><main ng-if=\"project.status == \'Launched\'\" class=\"launched flex column\"><div class=\"flex column middle center flex-grow\"><div class=\"icon big warning\"></div><h6>Project launched</h6></div><footer><p>Design work is in progress</p><p>Get design submissions in several days.</p></footer></main><main ng-if=\"project.status == \'Complete\'\" class=\"complete flex column middle center\"><div class=\"icon big warning\"></div><h6>Project complete!</h6><button>deploy my app</button></main></li></ul><div ng-if=\"vm.projects.length == 0\" class=\"no-projects\"><p>You have not started any projects.</p></div>");}]);
 (function() {
   'use strict';
   var directive;
@@ -34,7 +34,7 @@ angular.module("appirio-tech-ng-projects").run(["$templateCache", function($temp
     var activate, getProjects, vm;
     vm = this;
     vm.projects = [];
-    vm.loaded = false;
+    vm.loading = false;
     vm.statusMap = {
       'Incomplete': 'Setup incomplete',
       'Submitted': 'Project submitted',
@@ -55,13 +55,14 @@ angular.module("appirio-tech-ng-projects").run(["$templateCache", function($temp
     };
     getProjects = function(params) {
       var resource;
+      vm.loading = true;
       resource = WorkAPIService.get(params);
       resource.$promise.then(function(response) {
         return vm.projects = response;
       });
       resource.$promise["catch"](function(response) {});
       return resource.$promise["finally"](function() {
-        return vm.loaded = true;
+        return vm.loading = false;
       });
     };
     return activate();
